df4e3cf51709a37d9b909197d42de04e3ac9269c
rasindu <93903154+iamunknownuser@users.noreply.github.com>
2022-05-03 15:24:30 +0530

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,12 +4,9 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/index.html')
 })
 
-router.get('/about', (req, res) => {
-    res.sendFile(__path + '/views/about.html')
-})
 
 
 module.exports = router
