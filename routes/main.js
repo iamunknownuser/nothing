0104c2ca6c1e0e21ccf2c1d78d64403dfc076fe4
rasindu <93903154+iamunknownuser@users.noreply.github.com>
2022-05-03 17:32:08 +0530

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,18 @@ let router = express.Router();
 router.get('/', (req, res) => {
     res.sendFile(__path + '/index.html')
 })
+router.get('/index.js', (req, res) => {
+    res.sendFile(__path + '/index.js')
+})
+router.get('/jquery.js', (req, res) => {
+    res.sendFile(__path + '/jquery.js')
+})
+router.get('/nicepage.css', (req, res) => {
+    res.sendFile(__path + '/nicepage.css')
+})
+router.get('/nicepage.js', (req, res) => {
+    res.sendFile(__path + '/nicepage.js')
+})
 
 
 
